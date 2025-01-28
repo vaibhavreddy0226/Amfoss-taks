aeb8c03a33ac889d1728b187d9123fa239eef971
vaibhavreddy0226 <vaibhavreddymettupalli6@gmail.com>
2025-01-28 23:21:00 +0530

    push files

--- task-02/eval.py
+++ task-02/eval.py
@@ -1,16 +1,8 @@
 from PIL import Image
 import pytesseract
-
-# Load the image
 img = Image.open("expression_1.png")
-
-# Extract the text from the image
 extracted_text = pytesseract.image_to_string(img)
-
-# Clean the text to remove unwanted characters (like newlines)
 expression = extracted_text.strip()
-
-# Evaluate the expression
 try:
     result = eval(expression)
     print(f"The result of '{expression}' is: {result}")
